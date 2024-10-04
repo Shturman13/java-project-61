5afcffd80b1f4b146e585b14fe4632d0321357cd
dmitriiaristov <mitya.aristov@yandex.ru>
2024-10-04 13:59:00 +0300

    add getByName: build.gradle.kts, modified App.java add Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void sayHello() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
+
+
+
+}
